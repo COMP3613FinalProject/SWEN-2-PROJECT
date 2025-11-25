376925d0db8462c83faccb6c3764d75392b90cc8
Bry <bryannajodha@gmail.com>
2025-11-25 18:50:25 -0400

    Refactor Staff model by cleaning up code
    Removed unused Shortlist import and related methods.

--- App/models/staff.py
+++ App/models/staff.py
@@ -1,11 +1,9 @@
 from App.database import db
 from App.models.user import User
-from App.models.shortlist import Shortlist
 
 class Staff(User):
     __tablename__ = 'staff'
 
-    # Staff uses the User.id as its primary key
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
 
     __mapper_args__ = {
@@ -14,19 +12,7 @@ class Staff(User):
 
     def __init__(self, username, password, email, phone_number=None):
         super().__init__(username, password, email, phone_number)
-        self.type = 'staff'
+        self.type = 'staff'  # important for polymorphism
 
     def get_json(self):
-        base = super().get_json()
-        return base  # nothing extra to add
-
-    # Staff business logic
-    def add_to_shortlist(self, student_id, position_id):
-        shortlist = Shortlist(
-            student_id=student_id,
-            position_id=position_id,
-            staff_id=self.id
-        )
-        db.session.add(shortlist)
-        db.session.commit()
-        return shortlist
+        return super().get_json()
