2b8e0f640999d2672d08d3d3468249391109c441
JosephCait <caitlinjoseph5@gmail.com>
2025-11-25 16:16:49 -0400

    Task #46 - Implement Employer Controller
    Updates to decide_shortlist() to ensure correct decrement in number of positions and facilitate state transition through the Application model

--- App/controllers/employer.py
+++ App/controllers/employer.py
@@ -2,7 +2,6 @@ from App.models.employer import Employer
 from App.database import db
 from App.models import Student, Shortlist, Position
 from App.controllers.user import *
-from App.models.context import Context
 from App.models.application import Application
 
 def create_employer(username, password, email, company, phone_number):
@@ -15,10 +14,14 @@ def decide_shortlist(student_id, position_id, decision):
   student = db.session.query(Student).filter_by(user_id=student_id).first()
   shortlist = db.session.query(Shortlist).filter_by(student_id=student.id, position_id=position_id, status ="pending").first()
   position = db.session.query(Position).filter(Position.id==position_id, Position.number_of_positions > 0).first()
-  if shortlist and position:
+  
+  application = db.session.query(Application).filter_by(student_id=student.id, position_id=position.id).first()
+  
+  if shortlist and position and application:
     shortlist.update_status(decision)
-    position.update_number_of_positions(position.number_of_positions - 1)
-    db.session.commit()
+    if decision=="accept":
+      position.update_number_of_positions(position.number_of_positions - 1)
     application.setStatus(decision)
+    db.session.commit()
     return shortlist
   return False
