1280eb4e6361f3424e85fb76c6fc9ed078ee1450
JosephCait <caitlinjoseph5@gmail.com>
2025-11-26 16:42:44 -0400

    Task#15 - Implement new ShortListed state
    Implemented getMatchedCompanies()

--- App/models/shortlisted_state.py
+++ App/models/shortlisted_state.py
@@ -8,22 +8,30 @@ class ShortListedState(ApplicationState):
     def __init__(self):
         super().__init__("Shortlisted")
 
-    def next(self):
+    def next(self): # No automatic nect state, next state requires decision (parameter)
         return None
     
     def next_decision(self, decision: str): #Accepts decision from make_decision() in employer controller
         if decision == "accept":
             self.context.setState(AcceptedState())
-        elif decision == "rejected":
+        elif decision == "reject":
             self.context.setState(RejectedState())
 
     def previous(self):
         if self.context:
             self.context.setState(AppliedState())
+            self.context.application.status = "Applied"
+
+    def removeShortList(self);
+        return None #Not implemented - to be removed
 
     def withdraw(self):
         self.context.setState(RejectedState())
-        #application.setStatus("Withdrawn")
 
     def getMatchedCompanies(self):
-        super().getMatchedCompanies()
+        if not self.context or not self.context.application:
+            return []
+        student_id = self.context.application.student_id
+        #Applications that are in shortlisted state
+        shortlisted_pos = Application.query.filter_by(student_id=student_id, status="Shortlisted").all()
+        return [app.__repr__() for app in shortlisted_pos]
