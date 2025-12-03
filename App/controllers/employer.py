2765594779cd098423a40ea57afd1aa8b62ebc42
JosephCait <caitlinjoseph5@gmail.com>
2025-12-03 14:38:05 -0400

    Update employer.py

--- App/controllers/employer.py
+++ App/controllers/employer.py
@@ -8,20 +8,26 @@ def create_employer(username, password, email, company, phone_number):
   newEmployer = Employer(username, password, email, company, phone_number)
   db.session.add(newEmployer)
   db.session.commit()
+  print(f"Employer '{newEmployer.id}' created successfully!")
   return newEmployer
 
 def decide_shortlist(student_id, position_id, decision):
-  student = db.session.query(Student).filter_by(user_id=student_id).first()
-  shortlist = db.session.query(Shortlist).filter_by(student_id=student.id, position_id=position_id, status ="pending").first()
-  position = db.session.query(Position).filter(Position.id==position_id, Position.number_of_positions > 0).first()
-  
-  application = db.session.query(Application).filter_by(student_id=student.id, position_id=position.id).first()
-  
-  if shortlist and position and application:
-    shortlist.update_status(decision)
-    if decision=="accept":
-      position.update_number_of_positions(position.number_of_positions - 1)
-    application.setStatus(decision)
+
+  student = Student.query.get(student_id)
+  position = Position.query.filter(Position.id==position_id, Position.number_of_positions > 0).first()
+
+  if not student or not position:
+    return False
+
+  application = Application.query.filter_by(student_id=student.id, position_id=position.id).first()
+  if not application: return False
+    
+  shortlist = Shortlist.query.filter_by(application_id=application.id, isWithdrawn=False).first()
+
+  if shortlist and shortlist.application:
+    if decision == "accept":
+        position.update_number_of_positions(position.number_of_positions - 1)
+
+    shortlist.application.next(decision)
     db.session.commit()
     return shortlist
-  return False
