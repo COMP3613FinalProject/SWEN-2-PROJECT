c10df96545bbb1600b16c6d6db807241ba430892
Bry <bryannajodha@gmail.com>
2025-12-03 01:48:14 -0400

    Refactor staff creation and shortlist handling
    Refactor staff creation and shortlist logic for clarity and efficiency.

--- App/controllers/staff.py
+++ App/controllers/staff.py
@@ -14,13 +14,13 @@ def create_staff(username, password, email, phone_number=None):
     db.session.add(staff)
     db.session.commit()
 
-    print(f"Staff '{username}' created successfully!")
+    print(f"Staff '{staff.id}' created successfully!")
     return staff
 
 
 # 2. GET STAFF BY ID
 def get_staff(staff_id):
-    staff = Staff.query.filter_by(id=staff_id).first()
+    staff = Staff.query.get(staff_id)
     if not staff:
         print("Staff not found")
         return None
@@ -66,33 +66,33 @@ def staff_shortlist_student(staff_id, student_id, position_id):
         return None
 
     # Must be eligible
-    eligible_list, _ = get_eligible_students(position_id)
-    eligible_ids = [e["student_id"] for e in eligible_list] 
+    eligible_list= get_eligible_students(position_id)  #new change remove ,_
+    
+    eligible_ids = [e.id for e in eligible_list]  #new change # since get_eligible_students returns list of Student objects
 
     if student_id not in eligible_ids:
         print("Student does NOT meet GPA requirement")
         return None
 
     # Prevent duplicate shortlist
-    existing = Shortlist.query.filter_by(student_id=student_id, position_id=position_id).first()
+    existing = Shortlist.query.filter_by(application_id=app.id).first() # bc its composition 
     if existing:
         print("Student is already shortlisted")
         return None
 
     # Create shortlist entry
-    shortlist = Shortlist(
-        student_id=student_id,
-        position_id=position_id,
-        staff_id=staff_id
-    )
+    shortlist=Shortlist(application_id=app.id, staff_id=staff_id)
     db.session.add(shortlist)
-
-    # Update parent Application state
-    shortlist.setStatus("shortlisted")
 
     db.session.commit()
 
-    print(f"Student {student_id} successfully shortlisted for Position {position_id}")
+    if shortlist:
+        from App.models.shortlisted_state import ShortListedState
+        shortlist.application.set_state(ShortListedState())  # do i have to write commit here ?
+       
+
+
+    print(f"Shortlist {shortlist.application_id} created successfully for student {shortlist.application.student_id} for position {shortlist.application.position_id} by staff {shortlist.staff_id}")
     return shortlist
 
 
