05de98fc6f40487c6532dabe1a7444ee87016ad4
JosephCait <caitlinjoseph5@gmail.com>
2025-12-03 00:44:24 -0400

    Task #37 - Implement Accept Student API

--- App/views/employer.py
+++ App/views/employer.py
@@ -3,7 +3,6 @@
 Routes (blueprint url_prefix='/employer'):
 - POST   /employer/           -> create employer
 - POST   /employer/accept     -> employer accepts a shortlisted student (auth)
-- POST   /employer/reject     -> employer rejects a shortlisted student (auth)
 '''
 
 from flask import Blueprint, request, jsonify
@@ -59,34 +58,5 @@ def employer_accept_route():
             "internship_id": position_id,
             "student_id": student_id,
             "status": "accepted"
-        }
-    }), 200
-
-#Employer - reject student
-@employer_views.route('/reject', methods=['POST'])
-@jwt_required()
-def employer_reject_route():
-  """Employer rejects a shortlisted student
-  Body JSON: {"student_id": <int>, "position_id": <int>, "response": "Rejected"}
-  The employer_id is taken from the JWT identity (current user)"""
-
-  data = request.get_json() or {}
-  student_id = data.get("student_id")
-  position_id = data.get("position_id")
-  if not student_id or not position_id:
-    return jsonify({"error": "student_id and position_id required"}), 400
-
-  decision = "reject" #decision is always reject for this route
-  result = decide_shortlist(student_id, position_id, decision)
-  if not result:
-    return jsonify({"error": "Unable to reject student."}), 400
-
-  return jsonify({
-        "message": "Student rejected - successful",
-        "listing": {
-            "id": result.id,
-            "internship_id": position_id,
-            "student_id": student_id,
-            "status": "rejected"
         }
     }), 200
